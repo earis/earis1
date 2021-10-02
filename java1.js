830d48c03a302d4ac15368e085b6c149e82f3a32
earis <91765678+earis@users.noreply.github.com>
2021-10-02 16:28:55 +0800

    Update java1.js

--- java1.js
+++ java1.js
@@ -1,1 +1,10 @@
-alert("欢迎来到ZhangXin的主页！");
+alert("欢迎来到ZhangXin的主页！ 下面介绍有几个方面的内容：");
+var para,sss,tt;
+para = document.getElementsByClassName("boxtitle");
+for(i=0;i<para.length;i++)
+alert(i+1+": "+para[i].firstChild.nodeValue);
+sss = document.getElementById("main_container");
+tt = sss.getAttribute("title");
+alert(tt);
+sss.setAttribute("title","and interests included");
+alert(sss.getAttribute("title"));
